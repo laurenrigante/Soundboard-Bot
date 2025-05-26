fec3eac3e6f1aa464640fdca328bae50a15bf531
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2025-05-26 18:03:08 -0400

    register the command with discords servers ( currently running only private test server but will later be refactored)

---
+++ deploy-commands.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+const { REST, Routes } = require('discord.js');
+
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(path.join(commandsPath, file));
+  commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (slash) commands.');
+
+    await rest.put(
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), // for guild commands, faster for dev
+      { body: commands },
+    );
+
+    console.log('Successfully reloaded application (slash) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
