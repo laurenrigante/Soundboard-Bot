331061094405c14710b983e6c9c826713e72f5ef
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2025-06-03 10:53:34 -0400

    implementing sound service, which handles playing the sound in the voice channel.

--- services/soundService.js
+++ services/soundService.js
@@ -1,1 +1,49 @@
-//TODO: implementing sound playback logic in voice channel
+const {
+  createAudioPlayer,
+  createAudioResource,
+  AudioPlayerStatus,
+  entersState,
+} = require("@discordjs/voice");
+const getSoundPath = require("../utils/getSoundPath");
+
+let idleTimeout;
+
+async function playSound(connection, category, filename) {
+  const soundPath = getSoundPath(category, filename);
+
+  // clear the existing idle timeout when a new sound is clicked
+  if (idleTimeout) {
+    clearTimeout(idleTimeout);
+    idleTimeout = null;
+  }
+
+  const player = createAudioPlayer();
+  const resource = createAudioResource(soundPath);
+
+  player.play(resource);
+  connection.subscribe(player);
+
+  try {
+    await entersState(player, AudioPlayerStatus.Playing, 5_000);
+  } catch (error) {
+    console.error("Connection failed:", error);
+    connection.destroy();
+    throw new Error("Failed to connect to voice channel");
+  }
+
+  player.on(AudioPlayerStatus.Idle, () => {
+    console.log("Playback finished, starting 5 min idle timer...");
+
+    // Start 5 minute timer to disconnect if no new sound plays
+    idleTimeout = setTimeout(() => {
+      console.log("No sounds played for 5 minutes, leaving voice channel.");
+      connection.destroy();
+    }, 5 * 60 * 1000);
+  });
+
+  return;
+}
+
+module.exports = {
+  playSound,
+};
